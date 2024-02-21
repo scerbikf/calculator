07b1931cdf6bee6f439031fa0cc5607376744a18
scerbikf <filipscerbik487@gmail.com>
2024-02-21 13:12:08 +0100

    final

---
+++ index.js
@@ -0,0 +1,16 @@
+const display = document.getElementById("display");
+
+function addToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(eroor){
+        display.value = "Eroor";
+    }
+}
